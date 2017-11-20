a878d2178f3ab89c480877c2eaf62252e435c399
Phillip_Chan <phillip.chan@liferay.COM>
2017-11-20 15:19:57 -0800

    Run start commnad

--- server/index.js
+++ server/index.js
@@ -3,6 +3,16 @@
 'use strict';
 var express = require('express');
 var parser = require('body-parser');
+var cmd = require('node-cmd');
+
+cmd.get('npx cypress run', function(err, data, stderr) {
+	if (err) {
+		console.log('error:');
+		console.log(err);
+	}
+	console.log('the current dir contains these files :\n\n', data);
+	process.exit();
+});
 
 // create instance of the server to variable app
 var app = express();
@@ -14,11 +24,11 @@ app.use(parser.urlencoded({ extended: false }));
 app.use('/', express.static('client/'));
 
 // direct all other routes to client-side app
-app.all('/*', function (req, res) {
-    res
-        .status( 200 )
-        .set({'content-type': 'text/html; charset=utf-8'})
-        .sendFile(process.cwd() + '/client/index.html');
+app.all('/*', function(req, res) {
+	res
+		.status(200)
+		.set({ 'content-type': 'text/html; charset=utf-8' })
+		.sendFile(process.cwd() + '/client/index.html');
 });
 
 // have our app listen on port 3000
