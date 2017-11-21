bf35341f17fc57c6ad54c1a76f201261e63d2d5b
Phillip_Chan <phillip.chan@liferay.COM>
2017-11-21 15:49:51 -0800

    Remove sample test

--- cypress/integration/sample_spec.js
+++
@@ -1,7 +0,0 @@
-describe('random tests', function() {
-	it('should be true', function() {
-		cy.visit('http://www.liferay.com/request-a-demo');
-
-		cy.title().should('include', 'Kitchen Sink');
-	});
-});
