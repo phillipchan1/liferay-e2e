73852a5e6d006be2b493b3df31477e8b7c84a6a6
Phillip Chan <phillip.chan@liferay.com>
2017-12-01 10:55:06 -0800

    Updated test name

--- cypress/integration/hubspot_form_generator.js
+++ cypress/integration/hubspot_form_generator.js
@@ -1,23 +1,23 @@
-describe('There is a form on the page', function() {
-	it('the form should be there', function() {
-		cy.visit('http://www.liferay.com/request-a-demo');
-		cy.get('.lrdcom-form').should.exist;
-	});
+describe("There is a form on the page", function() {
+  it("the form should be there", function() {
+    cy.visit("http://www.liferay.com/request-a-demo");
+    cy.get(".lrdcom-form").should.exist;
+  });
 });
 
-describe('Form UI Works for Filling out Form', function() {
-	it('should show the thank you page upon finishing filling out the form', function() {
-		cy.visit('http://www.liferay.com/request-a-demo');
+describe("Filling out the form correctly should lead to thank you page", function() {
+  it("should show the thank you page upon finishing filling out the form", function() {
+    cy.visit("http://www.liferay.com/request-a-demo");
 
-		cy.get('#article244788_firstname input').type('TEST FIRST NAME');
-		cy.get('#article244788_lastname input').type('TEST LAST NAME');
-		cy.get('#article244788_email input').type('test@test.com');
-		cy.get('#article244788_phone input').type('5555555555');
-		cy.get('#article244788_company input').type('TEST COMPANY');
+    cy.get("#article244788_firstname input").type("TEST FIRST NAME");
+    cy.get("#article244788_lastname input").type("TEST LAST NAME");
+    cy.get("#article244788_email input").type("test@liferay.com");
+    cy.get("#article244788_phone input").type("5555555555");
+    cy.get("#article244788_company input").type("TEST COMPANY");
 
-		cy.get('#article244788_country select').select('United States'); // Select the 'user-1' option
+    cy.get("#article244788_country select").select("United States"); // Select the 'user-1' option
 
-		cy.get('.lrdcom-form').submit();
-		cy.url().should('include', 'thank-you');
-	});
+    cy.get(".lrdcom-form").submit();
+    cy.url().should("include", "thank-you");
+  });
 });
