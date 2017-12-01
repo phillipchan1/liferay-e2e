8da31d2919c599d4feebb1ba652ea8c2649e6634
Phillip Chan <phillip.chan@liferay.com>
2017-12-01 12:09:05 -0800

    UPdate schedule + SF

--- cypress/integration/hubspot_form_generator.js
+++ cypress/integration/hubspot_form_generator.js
@@ -1,5 +1,5 @@
 describe("There is a form on the page", function() {
-  it("the form should be there", function() {
+  it("should be there", function() {
     cy.visit("http://www.liferay.com/request-a-demo");
     cy.get(".lrdcom-form").should.exist;
   });
@@ -14,8 +14,7 @@ describe("Filling out the form correctly should lead to thank you page", functio
     cy.get("#article244788_email input").type("test@liferay.com");
     cy.get("#article244788_phone input").type("5555555555");
     cy.get("#article244788_company input").type("TEST COMPANY");
-
-    cy.get("#article244788_country select").select("United States"); // Select the 'user-1' option
+    cy.get("#article244788_country select").select("United States");
 
     cy.get(".lrdcom-form").submit();
     cy.url().should("include", "thank-you");
