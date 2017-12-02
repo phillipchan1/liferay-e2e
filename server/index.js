d9787705975d27e895d310342250d2989ecaba21
protoEvangelion <ryantgarant@gmail.com>
2017-12-01 17:04:55 -0800

    Prettier SF

--- server/index.js
+++ server/index.js
@@ -1,37 +1,35 @@
-/*jslint node: true */
+'use strict'
 
-'use strict';
-var express = require('express');
-var parser = require('body-parser');
-var cmd = require('node-cmd');
+const express = require('express')
+const parser = require('body-parser')
+const cmd = require('node-cmd')
 
 cmd.get('npx cypress run', function(err, data, stderr) {
 	if (err) {
-		console.log('error:');
-		console.log(err);
+		console.log('error:')
+		console.log(err)
 	}
-	console.log('the current dir contains these files :\n\n', data);
-	process.exit();
-});
+	console.log('the current dir contains these files :\n\n', data)
+	process.exit()
+})
 
-// create instance of the server to variable app
-var app = express();
+const app = express()
 
 // get method for parsing body
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json())
+app.use(parser.urlencoded({ extended: false }))
 
-app.use('/', express.static('client/'));
+app.use('/', express.static('client/'))
 
 // direct all other routes to client-side app
 app.all('/*', function(req, res) {
 	res
 		.status(200)
 		.set({ 'content-type': 'text/html; charset=utf-8' })
-		.sendFile(process.cwd() + '/client/index.html');
-});
+		.sendFile(process.cwd() + '/client/index.html')
+})
 
 // have our app listen on port 3000
 app.listen(process.env.PORT || 80, function() {
-	console.log('Service on running on 80');
-});
+	console.log('Service on running on 80')
+})
