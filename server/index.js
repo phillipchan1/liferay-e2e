77a6d7f815bc748340ac488305f13853482e40ae
protoEvangelion <ryantgarant@gmail.com>
2017-12-01 17:15:28 -0800

    Made server cross platform

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const parser = require('body-parser')
 const cmd = require('node-cmd')
 
-cmd.get('npx cypress run', function(err, data, stderr) {
+cmd.get('npx cypress run', function(err, data) {
 	if (err) {
 		console.log('error:')
 		console.log(err)
@@ -22,14 +22,21 @@ app.use(parser.urlencoded({ extended: false }))
 app.use('/', express.static('client/'))
 
 // direct all other routes to client-side app
-app.all('/*', function(req, res) {
-	res
+app.all('/*', function(request, response) {
+	response
 		.status(200)
 		.set({ 'content-type': 'text/html; charset=utf-8' })
 		.sendFile(process.cwd() + '/client/index.html')
 })
 
-// have our app listen on port 3000
-app.listen(process.env.PORT || 80, function() {
-	console.log('Service on running on 80')
+const port = process.env.PORT || 80
+
+app.listen(port, error => {
+	const boldBlue = text => `\u001b[1m\u001b[34m${text}\u001b[39m\u001b[22m`
+
+	if (error) {
+		console.error(error)
+	} else {
+		console.info(`Server is running at port ${boldBlue(port)}`)
+	}
 })
